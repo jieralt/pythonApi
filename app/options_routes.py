7db4c44d6bc9feff7086b081668ca4cbc2e54919
jieralt <jieralt@163.com>
2024-05-24 09:05:01 +0800

    t2

--- app/options_routes.py
+++ app/options_routes.py
@@ -1,35 +1,24 @@
 from flask import Blueprint, jsonify, request
-from flask_restx import Api, Resource, Namespace
 import os
+import json
 
 options_bp = Blueprint('options', __name__)
-api = Api(options_bp, doc='/swagger/')
+data_file = os.path.join('data', 'tasks.json')
 
-ns = Namespace('options', description='Options related operations')
-api.add_namespace(ns)
+# 初始化数据文件
+if not os.path.exists(data_file):
+    with open(data_file, 'w') as file:
+        json.dump([], file)
 
-data_file = os.path.join('data', 'selected_options.txt')
+@options_bp.route('/tasks', methods=['GET'])
+def get_tasks():
+    with open(data_file, 'r') as file:
+        tasks = json.load(file)
+    return jsonify(tasks)
 
-@ns.route('/submit-options')
-class SubmitOptions(Resource):
-    @ns.doc(params={'options': 'A list of selected options'})
-    def post(self):
-        '''Submits the selected options and saves them to a txt file'''
-        data = request.json
-        selected_options = data.get('options', [])
-        with open(data_file, 'a') as file:
-            for option in selected_options:
-                file.write(option + '\n')
-        return jsonify({'status': 'success', 'selected_options': selected_options})
-
-@ns.route('/get-options')
-class GetOptions(Resource):
-    def get(self):
-        '''Retrieves all submitted options from the txt file'''
-        if os.path.exists(data_file):
-            with open(data_file, 'r') as file:
-                options = file.readlines()
-            options = [option.strip() for option in options]
-        else:
-            options = []
-        return jsonify({'status': 'success', 'options': options})
+@options_bp.route('/tasks', methods=['POST'])
+def save_tasks():
+    tasks = request.json
+    with open(data_file, 'w') as file:
+        json.dump(tasks, file)
+    return jsonify({"status": "success"}), 200
